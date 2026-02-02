071402493bee694227650a9e1cdae0e7a21a8c0b
Amit Singh <singhamitchs19@gmail.com>
2026-02-02 19:59:47 +0530

    practice

---
+++ ShortesDistance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main() {
+    int n,m;
+    cin>>n>>m;
+    vector<vector<pair<int,int>>> adj(n);
+    for(int i = 0;i<m;i++){
+        int x,y,c;
+        cin>>x>>y>>c;
+        adj[x-1].push_back({y-1,c});
+    }
+    vector<int> dist(n,INT_MAX);
+    dist[0] = 0;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> p;
+    p.push({0,0});
+
+    while(!p.empty()){
+        auto [dis,node] = p.top();
+        p.pop();
+
+        for(auto [a,b] : adj[node]){
+            if(dist[a]>dis+b) {dist[a] = dis+b;p.push({dis+b,a});}
+        }
+    }
+    for(int i : dist) cout<<i<<" ";
+    cout<<endl;
+}
